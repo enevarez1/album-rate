c90d4219a89709b77a3cfac29d6b5ba1aeac2c82
Erick Nevarez <ericknevarez97@gmail.com>
2022-12-04 19:44:50 -0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/album_rate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.album_rate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/album.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Album {
+  String name;
+  String artist;
+  String year;
+  String genre;
+
+  Album({
+    required this.name, 
+    required this.artist, 
+    required this.year, 
+    required this.genre
+  });
+}
